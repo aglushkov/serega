4eef42c1b31e63423418137ca6972d3a6fd4903d
Andrey Glushkov <aglushkov@shakuro.com>
2022-06-26 20:18:58 +0300

    Add #to_json and #as_json methods

---
+++ lib/serega/utils/to_json.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class Serega
+  module Utils
+    class ToJSON
+      class << self
+        def call(data)
+          json_adapter.dump(data)
+        end
+
+        private
+
+        def json_adapter
+          @json_adapter ||= begin
+            require "json"
+            ::JSON
+          end
+        end
+      end
+    end
+  end
+end
