55a7dad6c611731f9e8621ef59af10a211f2e9e3
Andrey Glushkov <aglushkov@shakuro.com>
2023-04-20 00:15:08 +0300

    Better document :batch plugin

--- lib/serega/plugins/batch/batch.rb
+++ lib/serega/plugins/batch/batch.rb
@@ -3,23 +3,52 @@
 class Serega
   module SeregaPlugins
     #
-    # Plugin that can be used to load attributes values in batches.
-    #   Each batch loader accepts list of selected keys, context and current attribute point.
-    #   Each batch loader must return Hash with values grouped by provided keys.
-    #   There are specific `:default` option that can be used to add default value for missing key.
+    # Plugin `:batch`
+    #
+    # Adds ability to load nested attributes values in batches.
+    #
+    # It can be used to find value for attributes in optimal way:
+    # - load associations for multiple objects
+    # - load counters for multiple objects
+    # - make any heavy calculations for multiple objects only once
+    #
+    # After including plugin, attributes gain new `:batch` option.
+    #
+    # `:batch` option must be a hash with this keys:
+    # - `key` (required) [Symbol, Proc, callable] - Defines current object identifier.
+    #   Later `loader` will accept array of `keys` to find `values`.
+    # - `loader` (required) [Symbol, Proc, callable] - Defines how to fetch values for
+    #   batch of keys. Receives 3 parameters: keys, context, plan_point.
+    # - `default` (optional) - Default value for attribute.
+    #   By default it is `nil` or `[]` when attribute has option `many: true`
+    #
+    # If `:loader` was defined using name (as Symbol) then batch loader must be
+    # defined in serializer config: `config.batch.define(:loader_name) { ... }` method.
+    #
+    # *Result of this `:loader` callable must be a **Hash** where*:
+    # - keys - provided keys
+    # - values - values for according keys
+    #
+    # `Batch` plugin can be defined with two specific attributes:
+    # - `auto_hide: true` - Marks attributes with defined :batch as hidden, so it
+    #   will not be serialized by default
+    # - `default_key: :id` - Set default object key (in this case :id) that will be used for all attributes with :batch option specified.
+    #
+    # This options (`auto_hide`, `default_key`) also can be set as config options in
+    # any nested serializer.
     #
     # @example
     #   class PostSerializer < Serega
-    #     plugin :batch
+    #     plugin :batch, auto_hide: true, default_key: :id
     #
     #     # Define batch loader via callable class, it must accept three args (keys, context, plan_point)
-    #     attribute :comments_count, batch: { key: :id, loader: PostCommentsCountBatchLoader, default: 0}
+    #     attribute :comments_count, batch: { loader: PostCommentsCountBatchLoader, default: 0}
     #
     #     # Define batch loader via Symbol, later we should define this loader via config.batch.define(:posts_comments_counter) { ... }
-    #     attribute :comments_count, batch: { key: :id, loader: :posts_comments_counter, default: 0}
+    #     attribute :comments_count, batch: { loader: :posts_comments_counter, default: 0}
     #
     #     # Define batch loader with serializer
-    #     attribute :comments, serializer: CommentSerializer, batch: { key: :id, loader: :posts_comments, default: []}
+    #     attribute :comments, serializer: CommentSerializer, batch: { loader: :posts_comments, default: []}
     #
     #     # Resulted block must return hash like { key => value(s) }
     #     config.batch.define(:posts_comments_counter) do |keys|
