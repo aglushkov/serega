b0a2f88344f4b83294a704da02caf2a99575c8cc
Andrey Glushkov <aglushkov@shakuro.com>
2023-08-13 09:49:40 +0300

    More strict :many option validations
    Add validation that option :many can only be added only together with
    :serializer or :batch options, it is useless without them

--- lib/serega/validations/attribute/check_opt_many.rb
+++ lib/serega/validations/attribute/check_opt_many.rb
@@ -7,17 +7,34 @@ class Serega
       # Attribute `:many` option validator
       #
       class CheckOptMany
-        #
-        # Checks attribute :many option
-        #
-        # @param opts [Hash] Attribute options
-        #
-        # @raise [SeregaError] SeregaError that option has invalid value
-        #
-        # @return [void]
-        #
-        def self.call(opts)
-          Utils::CheckOptIsBool.call(opts, :many)
+        class << self
+          #
+          # Checks attribute :many option
+          #
+          # @param opts [Hash] Attribute options
+          #
+          # @raise [SeregaError] SeregaError that option has invalid value
+          #
+          # @return [void]
+          #
+          def call(opts)
+            return unless opts.key?(:many)
+
+            check_many_option_makes_sence(opts)
+            Utils::CheckOptIsBool.call(opts, :many)
+          end
+
+          private
+
+          def check_many_option_makes_sence(opts)
+            return if many_option_makes_sence?(opts)
+
+            raise SeregaError, "Option :many can be provided only together with :serializer or :batch option"
+          end
+
+          def many_option_makes_sence?(opts)
+            opts[:serializer] || opts[:batch]
+          end
         end
       end
     end
